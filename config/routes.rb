f6b0332bc8d39ec916719bf3ec9abbf5ff2c0bad
Laaveniya <lavaniya.lala@gmail.com>
2023-07-15 18:26:48 +0530

    feat: provide access based on roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-
-  root to: "welcome#index"
-  get 'welcome/index'
-  devise_for :users, controllers: { registrations: 'registrations', invitations: 'admin/invitations' }
-  resources :users
-  resources :schools do
-    get 'students', on: :member
+  authenticated :user, ->(user) { user.admin? || user.school_admin? } do
+    root to: "welcome#index"
+    get 'welcome/index'
+    devise_for :users, controllers: { registrations: 'registrations', invitations: 'admin/invitations' }
+    resources :users
+    resources :schools do
+      get 'students', on: :member
+    end
+    resources :enrollments
+    resources :course_batches
+    resources :courses
   end
-  resources :enrollments
-  resources :course_batches
-  resources :courses
-  get '/dashboard', to: 'students#dashboard'
-  post '/enroll_course_batch', to: 'students#enroll_course_batch'
+
+  authenticated :user, ->(user) { user.student? } do
+    get '/dashboard', to: 'students#dashboard'
+    post '/enroll_course_batch', to: 'students#enroll_course_batch'
+  end
 end
