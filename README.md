b600ec8e53f776097a47ee0c60aa9564132e93a4
Laaveniya <lavaniya.lala@gmail.com>
2023-07-18 21:50:18 +0530

    feat: add seeds and update readme

--- README.md
+++ README.md
@@ -43,7 +43,7 @@ elasticsearch
 
 ```
 
-alternatively
+alternatively with docker
 
 ```
 docker-compose -f docker-compose.yml up
@@ -56,7 +56,13 @@ docker-compose -f docker-compose.yml up
 rails server
 ```
 
-6. Access the application in your web browser at `http://localhost:3000`.
+6. Set up the database with seed database
+
+```
+rails db:create db:migrate db:seed
+```
+
+7. Access the application in your web browser at `http://localhost:3000`.
 
 ## Running the Test Suite
 
@@ -86,3 +92,42 @@ The School Management System defines three user types with specific responsibili
 - Students can raise a request to enrol in a batch.
 - Students from the same batch can see their classmates and their progress.
 
+## Routes
+
+The application provides the following routes:
+
+### Authentication Routes
+
+- `GET /users/sign_in` - User sign-in page
+- `DELETE /users/sign_out` - User sign-out
+
+### Admin and School Admin Routes
+
+- `GET /welcome/index` - Home page for admin and school admin users
+- `GET /users` - List of users (admin and school admin for schools administered only)
+- `GET /users/:id` - User details (admin and school admin for schools administered only)
+- `PUT /users/:id` - Update user (admin only and school admin for schools administered only)
+- `DELETE /users/:id` - Delete user (admin only and school admin for schools administered only)
+- `GET /schools` - List of schools (admin and school admin for schools administered only)
+- `GET /schools/:id` - School details (admin and school admin for schools administered only)
+- `GET /schools/:id/students` - List of students in a school (admin and school admin)
+- `GET /enrollments` - List of enrollments (admin and school admin)
+- `GET /enrollments/:id` - Enrollment details (admin and school admin)
+- `PUT /enrollments/:id` - Update enrollment (admin and school admin)
+- `DELETE /enrollments/:id` - Delete enrollment (admin and school admin)
+- `GET /course_batches` - List of course batches (admin and school admin)
+- `GET /course_batches/:id` - Course batch details (admin and school admin)
+- `PUT /course_batches/:id` - Update course batch (admin and school admin)
+- `DELETE /course_batches/:id` - Delete course batch (admin and school admin)
+- `GET /courses` - List of courses (admin and school admin)
+- `GET /courses/:id` - Course details (admin and school admin)
+- `PUT /courses/:id` - Update course (admin and school admin)
+- `DELETE /courses/:id` - Delete course (admin and school admin)
+- `POST /users/invitation/new` - Create new Invitation (admin and school admin)
+
+### Student Routes
+
+- `GET /dashboard` - Student dashboard
+- `POST /enroll_course_batch` - Enroll in a course batch
+
+Please note that only authenticated users with the respective user types can access these routes.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create Admin user
+admin = User.create(email: 'admin@example.com', password: 'password', role: 'admin', name: 'Admin')
+
+# Create SchoolAdmin user
+school_admin = User.create(email: 'school_admin_1@example.com', password: 'password', role: 'school_admin', name: 'School Admin')
+school_admin_2 = User.create(email: 'school_admin_2@example.com', password: 'password', role: 'school_admin', name: 'School Admin 2')
+
+# Create Schools
+school1 = School.create(name: 'School 1', address: 'Address 1', creator_id: admin.id)
+school2 = School.create(name: 'School 2', address: 'Address 2', creator_id: admin.id)
+
+# CreateAdminships
+Adminship.create(school_id: school1.id, user_id: school_admin.id)
+Adminship.create(school_id: school2.id, user_id: school_admin_2.id)
+
+# Create Student users
+student1 = User.create(email: 'student1@example.com', password: 'password', role: 'student', name: 'Student 1')
+student2 = User.create(email: 'student2@example.com', password: 'password', role: 'student', name: 'Student 2')
+student3 = User.create(email: 'student3@example.com', password: 'password', role: 'student', name: 'Student 3')
+student4 = User.create(email: 'student4@example.com', password: 'password', role: 'student', name: 'Student 4')
+
+# Create SchoolMemberships
+SchoolMembership.create(user_id: student1.id, school_id: school1.id, active: true)
+SchoolMembership.create(user_id: student2.id, school_id: school1.id, active: true)
+SchoolMembership.create(user_id: student3.id, school_id: school2.id, active: true)
+SchoolMembership.create(user_id: student4.id, school_id: school2.id, active: true)
+
+# Create Courses
+course1 = Course.create(name: 'Course 1', school: school1, description: 'Description 1', creator_id: school_admin.id, start_date: Date.current, end_date: Date.current + 30.days)
+course2 = Course.create(name: 'Course 2', school: school2, description: 'Description 2', creator_id: admin.id, start_date: Date.current, end_date: Date.current + 30.days)
+
+# Create Batches
+batch1 = CourseBatch.create(name: 'Batch 1', course: course1, creator_id: school_admin.id, max_enrollment_count: 10, start_date: Date.current, end_date: Date.current + 30.days)
+batch2 = CourseBatch.create(name: 'Batch 2', course: course1, creator_id: school_admin.id, max_enrollment_count: 10, start_date: Date.current, end_date: Date.current + 30.days)
+batch3 = CourseBatch.create(name: 'Batch 3', course: course2, creator_id: admin.id, max_enrollment_count: 10, start_date: Date.current, end_date: Date.current + 30.days)
+batch4 = CourseBatch.create(name: 'Batch 4', course: course2, creator_id: admin.id, max_enrollment_count: 10, start_date: Date.current, end_date: Date.current + 30.days)
+
+# Create Enrollments (Request to Enrol in Batch)
+enrollment1 = Enrollment.create(student: student1, course_batch_id: batch1.id, status: 'requested')
+enrollment2 = Enrollment.create(student: student2, course_batch_id: batch1.id, status: 'requested')
+Enrollment.create(student: student3, course_batch_id: batch3.id, status: 'requested')
+Enrollment.create(student: student4, course_batch_id: batch4.id, status: 'denied')
+
+# Approve Enrollments
+enrollment1.update(status: 'approved', approver_id: school_admin)
+enrollment2.update(status: 'approved', approver_id: school_admin)
